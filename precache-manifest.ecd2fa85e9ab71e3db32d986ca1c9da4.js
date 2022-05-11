1609905031e9da4a76196fa4abee9f9ea374f4cb
windiewhale <feketebodza@gmail.com>
2022-05-11 18:27:36 +0200

    Updates

---
+++ precache-manifest.ecd2fa85e9ab71e3db32d986ca1c9da4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eda56fb8e0a374a3d831",
+    "url": "/w-18-test-exam-ui/static/css/main.dbaedf62.chunk.css"
+  },
+  {
+    "revision": "eda56fb8e0a374a3d831",
+    "url": "/w-18-test-exam-ui/static/js/main.eda56fb8.chunk.js"
+  },
+  {
+    "revision": "a59e3c495ff0be451ccd",
+    "url": "/w-18-test-exam-ui/static/js/1.a59e3c49.chunk.js"
+  },
+  {
+    "revision": "a1ae754edca04d9e8ac6",
+    "url": "/w-18-test-exam-ui/static/js/runtime~main.a1ae754e.js"
+  },
+  {
+    "revision": "22c3ab37ec47dfeb02340ca0fcac67fa",
+    "url": "/w-18-test-exam-ui/index.html"
+  }
+];
